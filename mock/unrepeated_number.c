2c757d087156e102a09650cea4ee7ae6923104b9
Yashas B K <yashas.bk@thundersoft.com>
2022-06-13 18:10:18 +0530

    Corrected the find unrepeated number program

--- mock/unrepeated_number.c
+++ mock/unrepeated_number.c
@@ -1,22 +1,28 @@
-//{1,2,3,4,2,4,5,3,1} = 5
-//Given an array of sequence of numbers which are repeated twice. In that one number is not repeated.
+//Given an array of numbers which are repeated twice. In that one number is not repeated.
 // Write a program to get that unrepeated number at a complexity of O(n).
+/*
+Sample input and output: 
+Scenario 1:
+Input = {1,2,3,7,3,2,1}
+Output = 7;
+
+Scenario 2:
+Input :{1,2,2,2,4,2,1}
+Output: 4
+*/
+
 
 #include <stdio.h>
 
 int main()
 {
 
-    int arr[] = {1,2,3,4,2,4,3,5,1,5,6}; // 6
+    int arr[] = {1,2,2,2,4,2,1}; 
     int len = sizeof(arr)/sizeof(int);
-    int main_sum = 0,seq_sum=0; 
-    for(int i=0;i<len;i++)
+    int out=arr[0];
+    for(int i=1;i<len;i++)
     {
-        if(i<len/2)
-        {
-            main_sum += i+1;
-        }
-        seq_sum += arr[i];
+        out = out ^ arr[i];
     }
-    printf("The unrepeated number in the array is %d",seq_sum-(main_sum*2));
+    printf("The unrepeated number in the array is %d",out);
 }
